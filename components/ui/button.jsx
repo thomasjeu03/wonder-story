a946b47a8ae38e83f6aa6fba45b32ce8d0534aa4
Thomas Jeu <jeuthomas03@gmail.com>
2024-08-23 00:56:13 +0200

    debut style Tailwind

--- components/ui/button.jsx
+++ components/ui/button.jsx
@@ -4,7 +4,7 @@ import { cva } from "class-variance-authority";
 import {cn} from "@/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex items-center gap-3 justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
@@ -20,10 +20,10 @@ const buttonVariants = cva(
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
-        default: "h-9 px-4 py-2",
-        sm: "h-8 rounded-md px-3 text-xs",
-        lg: "h-10 rounded-md px-8",
-        icon: "h-9 w-9",
+        sm: "rounded-md px-2 py-2 text-xs",
+        default: "px-3 py-3",
+        lg: "rounded-xxl px-6 py-6 text-xl font-semibold",
+        icon: "px-1 py-1",
       },
     },
     defaultVariants: {
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,10 @@ module.exports = {
             padding: "2rem",
             screens: {
                 "2xl": "1400px",
+                "xl": "1200px",
+                "m": "1024",
+                "s": "767px",
+                "xs": "480px",
             },
         },
         extend: {
