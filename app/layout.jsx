057e934dca871736c5d5e36eecb090fd9e2056b7
ThomasJeu <jeu@caddev.fr>
2024-08-23 13:26:35 +0200

    theme color system device

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,15 +5,18 @@ import {cn} from "@/lib/utils";
 import {ThemeProvider} from "@/components/theme-provider";
 import {SessionProvider} from "next-auth/react";
 import {UserProvider} from "@/app/contexts/UserContext";
+import {useTheme} from "next-themes";
 
 export default function RootLayout({ children, session }) {
+    const { theme } = useTheme()
     return (
-        <html lang="fr">
-            <body className={cn("min-h-screen font-sans antialiased")}>
+        <html lang="fr" className={theme}>
+            <body className={cn("min-h-dvh bg-background text-foreground font-sans antialiased")}>
                 <SessionProvider session={session}>
                     <ThemeProvider
                         attribute="class"
                         defaultTheme="system"
+                        enableSystem
                         disableTransitionOnChange
                     >
                         <UserProvider>
