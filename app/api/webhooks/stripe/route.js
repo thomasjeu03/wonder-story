b1a3384b60b59ae60a134353824458407191be18
Thomas Jeu <jeuthomas03@gmail.com>
2024-08-23 20:37:06 +0200

    layout MenuBar 3

--- app/api/webhooks/stripe/route.js
+++ app/api/webhooks/stripe/route.js
@@ -1,7 +1,7 @@
 import prisma from "../../../../lib/prisma";
 import {NextResponse} from "next/server";
-import {resend} from "@/lib/resend";
-import {TrialEndTemplate} from "@/components/email/trial-end-template";
+// import {resend} from "@/lib/resend";
+// import {TrialEndTemplate} from "@/components/email/trial-end-template";
 
 exports.POST = async (req) => {
     const body = await req.json();
@@ -83,18 +83,18 @@ exports.POST = async (req) => {
             });
             break;
         }
-        case "customer.subscription.trial_will_end": {
-            const subscription = body.data.object;
-            const stripeCustomerId = subscription.customer;
-            const user = await findUserFromCustomer(stripeCustomerId);
-
-            if (!user?.id) {
-                break;
-            }
-
-            await sendTrialEndNotification(user.email, user.name);
-            break;
-        }
+        // case "customer.subscription.trial_will_end": {
+        //     const subscription = body.data.object;
+        //     const stripeCustomerId = subscription.customer;
+        //     const user = await findUserFromCustomer(stripeCustomerId);
+        //
+        //     if (!user?.id) {
+        //         break;
+        //     }
+        //
+        //     await sendTrialEndNotification(user.email, user.name);
+        //     break;
+        // }
         default:
             console.log(`Unhandled event type ${body.type}`);
     }
@@ -119,24 +119,24 @@ const findUserFromCustomer = async (stripeCustomerId) => {
 // TODO: Envoi email notification when:
 //  -customer.subscription.created
 //  -checkout.session.completed
-const sendTrialEndNotification = async (email, name) => {
-    if (!email) {
-        return;
-    }
-
-    const msg = {
-        from: 'Thomas <contact@wonder-story.app>',
-        to: [email],
-        subject: 'Votre période d’essai se termine bientôt',
-        react: TrialEndTemplate({ name: name }),
-    };
-
-    try {
-        await resend.emails.send(msg);
-    } catch (error) {
-        console.error("Error sending trial end notification", error);
-        if (error.response) {
-            console.error(error.response.body);
-        }
-    }
-};
+// const sendTrialEndNotification = async (email, name) => {
+//     if (!email) {
+//         return;
+//     }
+//
+//     const msg = {
+//         from: 'Thomas <contact@wonder-story.app>',
+//         to: [email],
+//         subject: 'Votre période d’essai se termine bientôt',
+//         react: TrialEndTemplate({ name: name }),
+//     };
+//
+//     try {
+//         await resend.emails.send(msg);
+//     } catch (error) {
+//         console.error("Error sending trial end notification", error);
+//         if (error.response) {
+//             console.error(error.response.body);
+//         }
+//     }
+// };
