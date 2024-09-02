7e9168572de38bcbf249c88fbd6473c1392fd3d3
Thomas Jeu <jeuthomas03@gmail.com>
2024-09-02 22:04:01 +0200

    wip : landing page

--- components/LandingPage/LandingPage.jsx
+++ components/LandingPage/LandingPage.jsx
@@ -1,11 +1,13 @@
 import {Button} from "@/components/ui/button";
 import {signIn} from "next-auth/react";
+import Link from 'next/link';
 
 import logoWhite from   "../../public/img/logo-white.png";
 import bgLandingPage from   "../../public/img/bg-landing-page.png";
 import mockup from   "../../public/img/mockup.png";
 import premiumCard from   "../../public/img/premiumCard.png";
 import bgPremiumCard from   "../../public/img/bg-premium-card.png";
+import bgFooter from   "../../public/img/footer.png";
 import Image from "next/image";
 import {useLocale} from "@/app/contexts/LocaleContext";
 import {Sparkles, XIcon, CheckIcon} from "lucide-react";
@@ -16,10 +18,27 @@ export default function LandingPage() {
     return (
         <>
             <header className="flex flex-col items-center justify-start w-full pt-52">
-                <Image src={bgLandingPage} priority quality={100} width={2000} className="pointer-events-none absolute top-0 left-0 w-full h-auto -z-10"
+                <Image src={bgLandingPage} priority quality={100} width={2000}
+                       className="pointer-events-none absolute top-0 left-0 w-full h-auto -z-10"
                        alt='background image Wonder Story landing page'/>
                 <nav
-                    className="w-full flex items-center justify-center fixed top-0 z-50 left-0 border-b border-gray-700 bg-background/70 backdrop-blur-lg">
+                    className="w-full flex items-center justify-center fixed top-0 z-50 left-0">
+                    <div className="gradient-blur -z-10 h-24 md:h-32">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div className="gradient-blur-reverse -z-10 h-16 lg:h-32">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
                     <div className="w-full p-3 flex justify-between items-center max-w-5xl">
                         <a href="/">
                             <Image src={logoWhite} height={'34'} width={'183'} priority quality={100}
@@ -27,17 +46,17 @@ export default function LandingPage() {
                         </a>
                         <ul className="flex flex-row gap-8 items-center justify-end">
                             <li>
-                                <a href="/#benefits" className="text-gray-400 hover:text-gray-200">{t('benefits')}</a>
+                                <a href="/#benefits" className="text-gray-200 hover:text-gray-50">{t('benefits')}</a>
                             </li>
                             <li>
                                 <a href="/#how-it-works"
-                                   className="text-gray-400 hover:text-gray-200">{t('how-it-work')}</a>
+                                   className="text-gray-200 hover:text-gray-50">{t('how-it-work')}</a>
                             </li>
                             <li>
-                                <a href="/#price" className="text-gray-400 hover:text-gray-200">{t('price')}</a>
+                                <a href="/#price" className="text-gray-200 hover:text-gray-50">{t('price')}</a>
                             </li>
                             <li>
-                                <a href="/#faq" className="text-gray-400 hover:text-gray-200">{t('faqs')}</a>
+                                <a href="/#faq" className="text-gray-200 hover:text-gray-50">{t('faqs')}</a>
                             </li>
                             <li>
                                 <Button onClick={() => signIn("google")}>{t('login')}</Button>
@@ -97,8 +116,8 @@ d
                         <h2 className="text-center font-handwriting text-5xl text-balance">{t('section-price-title')}</h2>
                         <h3 className="text-center text-2xl text-gray-400 text-balance">{t('section-price-subtitle')}</h3>
                     </div>
-                    <div className="w-full grid grid-cols-3 gap-6">
-                        <div className="price-card">
+                    <div className="w-full grid grid-cols-1 md:grid-cols-3 gap-6">
+                        <div className="price-card max-w-md m-auto">
                             <div className="flex flex-col items-center justify-center gap-3 w-full">
                                 <h4 className='font-handwriting text-gray-300 text-3xl w-full'>{t('free_trial')}</h4>
                                 <p className="text-gray-200">
@@ -123,7 +142,7 @@ d
                             </Button>
                         </div>
                         <div
-                            className="price-card price-card--premium col-span-2 p-0 flex flex-row overflow-hidden relative">
+                            className="price-card price-card--premium col-auto md:col-span-2 p-0 flex flex-row overflow-hidden relative">
                             <Image src={bgPremiumCard} priority quality={100}
                                    className="absolute h-full top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 -z-10 saturate-150"
                                    alt='Wonder Story premium price'/>
@@ -168,8 +187,12 @@ d
                     </div>
                 </section>
             </main>
-            <footer>
-                Footer
+            <footer className='flex flex-col w-full'>
+                {/*TODO: all extras pages*/}
+                <Link href="https://thomasjeu.fr" target='_blank' rel='noopener' className="w-full text-center text-gray-400 text-sm">{t('made_by')} Thomas JEU</Link>
+                <Image src={bgFooter} priority quality={100}
+                       className="w-full h-auto relative"
+                       alt='Wonder Story footer image'/>
             </footer>
         </>
     );
