7f052bc4b5681c28cdc48a5a145ea77e78f4052b
Thomas Jeu <jeuthomas03@gmail.com>
2024-09-10 20:57:47 +0200

    landing page V1

--- components/LandingPage/LandingPage.jsx
+++ components/LandingPage/LandingPage.jsx
@@ -8,18 +8,20 @@ import mockup from   "../../public/img/mockup.png";
 import premiumCard from   "../../public/img/premiumCard.png";
 import bgPremiumCard from   "../../public/img/bg-premium-card.png";
 import bgFooter from   "../../public/img/footer.png";
+import benefit1 from   "../../public/img/landingPage/benefit1.png";
+import benefit2 from   "../../public/img/landingPage/benefit2.png";
 import Image from "next/image";
 import {useLocale} from "@/app/contexts/LocaleContext";
-import {Sparkles, XIcon, CheckIcon} from "lucide-react";
+import {Sparkles, XIcon, CheckIcon, Timer, Globe} from "lucide-react";
 
 export default function LandingPage() {
     const {t} = useLocale();
     // TODO: landing page
     return (
         <>
-            <header className="flex flex-col items-center justify-start w-full pt-52">
+            <header className="flex flex-col items-center justify-start w-full pt-36 md:pt-52">
                 <Image src={bgLandingPage} priority quality={100} width={2000}
-                       className="pointer-events-none absolute top-0 left-0 w-full h-auto -z-10"
+                       className="pointer-events-none absolute top-0 left-0 w-full h-dvh md:h-auto -z-10"
                        alt='background image Wonder Story landing page'/>
                 <nav
                     className="w-full flex items-center justify-center fixed top-0 z-50 left-0">
@@ -45,19 +47,19 @@ export default function LandingPage() {
                                    alt='Wonder Studio logo white'/>
                         </a>
                         <ul className="flex flex-row gap-8 items-center justify-end">
-                            <li>
-                                <a href="/#benefits" className="text-gray-200 hover:text-gray-50">{t('benefits')}</a>
-                            </li>
-                            <li>
-                                <a href="/#how-it-works"
-                                   className="text-gray-200 hover:text-gray-50">{t('how-it-work')}</a>
-                            </li>
-                            <li>
-                                <a href="/#price" className="text-gray-200 hover:text-gray-50">{t('price')}</a>
-                            </li>
-                            <li>
-                                <a href="/#faq" className="text-gray-200 hover:text-gray-50">{t('faqs')}</a>
-                            </li>
+                            {/*<li>*/}
+                            {/*    <a href="/#benefits" className="text-gray-200 hover:text-gray-50">{t('benefits')}</a>*/}
+                            {/*</li>*/}
+                            {/*<li>*/}
+                            {/*    <a href="/#how-it-works"*/}
+                            {/*       className="text-gray-200 hover:text-gray-50">{t('how-it-work')}</a>*/}
+                            {/*</li>*/}
+                            {/*<li>*/}
+                            {/*    <a href="/#price" className="text-gray-200 hover:text-gray-50">{t('price')}</a>*/}
+                            {/*</li>*/}
+                            {/*<li>*/}
+                            {/*    <a href="/#faq" className="text-gray-200 hover:text-gray-50">{t('faqs')}</a>*/}
+                            {/*</li>*/}
                             <li>
                                 <Button onClick={() => signIn("google")}>{t('login')}</Button>
                             </li>
@@ -65,9 +67,9 @@ export default function LandingPage() {
                     </div>
                 </nav>
                 <h1 className="sr-only">Wonder Story - Générateur d histoires pour enfants</h1>
-                <div className="flex flex-col items-center gap-12 w-full max-w-2xl">
+                <div className="flex flex-col items-center gap-12 w-full max-w-3xl">
                     <div className="flex flex-col items-center gap-6 w-full">
-                        <h2 className="text-center font-semibold text-5xl text-balance">{t('landing-hero-title')}</h2>
+                        <h2 className="text-center font-semibold text-3xl sm:text-4xl md:text-5xl lg:text-7xl text-balance">{t('landing-hero-title')}</h2>
                         <h3 className="text-center text-gray-400 text-balance max-w-xl">{t('landing-hero-subtitle')}</h3>
                     </div>
                     <Button size="lg" onClick={() => signIn("google")}>
@@ -75,46 +77,75 @@ export default function LandingPage() {
                         <Sparkles/>
                     </Button>
                 </div>
-                {/*mockup fait sur shots.so*/}
-                {/*TODO: refaire le mockup when app is ready*/}
-                <Image src={mockup} priority quality={100} width={1000} className="mt-16"
-                       alt='Wonder Story mockup'/>
+                <Image src={mockup} priority quality={100} width={1300} alt='Wonder Story mockup'/>
             </header>
             <main className="w-full flex flex-col justify-start items-center gap-48 mt-48 mb-48">
                 <section id='benefits' className="w-full flex-col gap-12 flex max-w-5xl">
                     <div className="w-full flex flex-col gap-4">
-                        <h2 className="font-handwriting text-5xl">{t('section-benefits-title')}</h2>
-                        <h3 className="text-2xl text-gray-400">{t('section-benefits-subtitle')}</h3>
+                        <h2 className="font-handwriting text-5xl lg:text-7xl">{t('section-benefits-title')}</h2>
+                        <h3 className="text-md md:text-2xl text-gray-400">{t('section-benefits-subtitle')}</h3>
                     </div>
-                    <div>
-d
+                    <div className="w-full grid grid-cols-1 md:grid-cols-3 gap-6">
+                        <div className="price-card col-auto md:col-span-2 m-auto p-0 gap-0">
+                            <div className="flex flex-col items-center justify-center gap-3 w-full p-6">
+                                <h4 className='font-handwriting text-gray-300 text-4xl w-full flex flex-row gap-3'>
+                                    <Timer size={32}/>
+                                    {t('dont-waste-time')}
+                                </h4>
+                                <p className="text-gray-200 w-full">
+                                    {t('your-story-in-less-than-a-minute')}
+                                </p>
+                            </div>
+                            <Image src={benefit1} priority quality={100}
+                                   className="w-full h-auto"
+                                   alt='Wonder Story benefit 1'/>
+                        </div>
+                        <div className="price-card col-auto md:col-span-1 p-0 gap-0 relative min-h-96">
+                            <div className="gradient-blur z-0 h-48 md:h-32">
+                                <div></div>
+                                <div></div>
+                                <div></div>
+                                <div></div>
+                                <div></div>
+                                <div></div>
+                            </div>
+                            <div className="flex flex-col gap-3 w-full p-6 z-10">
+                                <h4 className='font-handwriting text-gray-300 text-4xl w-full flex flex-row gap-3'>
+                                    <Globe size={32}/>
+                                    {t('benefit-2-title')}
+                                </h4>
+                                <p>{t('benefit-2')}</p>
+                            </div>
+                            <Image src={benefit2} priority quality={100}
+                                   className="w-full h-auto absolute bottom-0 left-0 right-0 -z-10"
+                                   alt='Wonder Story benefit 2'/>
+                        </div>
                     </div>
+                    <p className="text-gray-300 text-center w-full">{t('benefit-3')}</p>
                 </section>
 
-                <section id="how-it-works" className="w-full gap-12 flex flex-col items-center max-w-5xl">
-                    <div className="w-full flex flex-col gap-4">
-                        <h2 className="text-center font-handwriting text-5xl">{t('section-steps-title')}</h2>
-                        <h3 className="text-center text-2xl text-gray-400">{t('section-steps-subtitle')}</h3>
-                    </div>
-                    <div>
-d
-                    </div>
-                </section>
+                {/*<section id="how-it-works" className="w-full gap-12 flex flex-col items-center max-w-5xl">*/}
+                {/*    <div className="w-full flex flex-col gap-4">*/}
+                {/*        <h2 className="text-center font-handwriting text-5xl lg:text-7xl">{t('section-steps-title')}</h2>*/}
+                {/*        <h3 className="text-center text-md md:text-2xl text-gray-400">{t('section-steps-subtitle')}</h3>*/}
+                {/*    </div>*/}
+                {/*    <div>*/}
+                {/*    </div>*/}
+                {/*</section>*/}
 
-                <section id='testimonials' className="w-full gap-12 flex flex-col max-w-5xl">
-                    <div className="w-full flex flex-col gap-4">
-                        <h2 className="font-handwriting text-5xl">{t('section-testimonials-title')}</h2>
-                        <h3 className="text-2xl text-gray-400">{t('section-testimonials-subtitle')}</h3>
-                    </div>
-                    <div>
-d
-                    </div>
-                </section>
+                {/*<section id='testimonials' className="w-full gap-12 flex flex-col max-w-5xl">*/}
+                {/*    <div className="w-full flex flex-col gap-4">*/}
+                {/*        <h2 className="font-handwriting text-5xl lg:text-7xl">{t('section-testimonials-title')}</h2>*/}
+                {/*        <h3 className="text-md md:text-2xl text-gray-400">{t('section-testimonials-subtitle')}</h3>*/}
+                {/*    </div>*/}
+                {/*    <div>*/}
+                {/*    </div>*/}
+                {/*</section>*/}
 
                 <section id='price' className="w-full gap-12 flex flex-col justify-start items-start max-w-5xl">
                     <div className="w-full flex flex-col gap-4">
-                        <h2 className="text-center font-handwriting text-5xl text-balance">{t('section-price-title')}</h2>
-                        <h3 className="text-center text-2xl text-gray-400 text-balance">{t('section-price-subtitle')}</h3>
+                        <h2 className="text-center font-handwriting text-5xl lg:text-7xl text-balance">{t('section-price-title')}</h2>
+                        <h3 className="text-center text-md md:text-2xl text-gray-400 text-balance">{t('section-price-subtitle')}</h3>
                     </div>
                     <div className="w-full grid grid-cols-1 md:grid-cols-3 gap-6">
                         <div className="price-card max-w-md m-auto">
@@ -148,9 +179,10 @@ d
                                    alt='Wonder Story premium price'/>
                             <div className="flex flex-col gap-12 w-full p-6">
                                 <div className="flex flex-col items-center justify-center gap-3 w-full">
-                                <h4 className='font-handwriting text-gray-300 text-3xl w-full'>Premium</h4>
+                                    <h4 className='font-handwriting text-gray-300 text-3xl w-full'>Premium</h4>
                                     <p className="text-gray-200">
-                                        <span className="font-handwriting text-5xl text-foreground">4,99€</span> /{t('month')}
+                                        <span
+                                            className="font-handwriting text-5xl text-foreground">4,99€</span> /{t('month')}
                                     </p>
                                     <p className="text-gray-300 text-sm text-center">{t('subtitle-card-premium')}</p>
                                 </div>
