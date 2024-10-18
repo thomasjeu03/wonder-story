e648bfdd3f0bbd8160c84fe781d4480bcd5f44a6
Thomas JEU <70963286+thomasjeu03@users.noreply.github.com>
2024-10-18 13:47:34 +0200

    ads.txt added

--- app/page.jsx
+++ app/page.jsx
@@ -15,6 +15,7 @@ export default function Home() {
                     dataAdSlot="8653074776"
                 />
             </div>
+            
             <HomeContent />
         </>
     );
